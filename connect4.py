336a8fd0a4444a78c71d12db2c595884c792f1f1
alihussainzawriya2007 <ali.hussainzawriya2025@vitstudent.ac.in>
2025-11-08 12:41:04 +0530

    Delete connect4.py

--- connect4.py
+++
@@ -1,9 +0,0 @@
-
-class Board:
-
-    def __init__(self, rows=6, cols=7):
-        self.rows = rows
-        self.cols = cols
-        self.grid:list[list[str]] = [[' ' for _ in range(cols)] for _ in range(rows)]
-
-    
